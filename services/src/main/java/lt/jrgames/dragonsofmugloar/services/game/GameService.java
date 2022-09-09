81325d16a940e4e2c20a66ba316e2f44b94748d2
Justas Ratkevicius <justas.ratkevicius@gmail.com>
2022-09-09 14:52:12 +0300

    Simplified request mapping

--- services/src/main/java/lt/jrgames/dragonsofmugloar/services/game/GameService.java
+++ services/src/main/java/lt/jrgames/dragonsofmugloar/services/game/GameService.java
@@ -2,12 +2,10 @@ package lt.jrgames.dragonsofmugloar.services.game;
 
 import lt.jrgames.dragonsofmugloar.services.game.model.Game;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "GameService", url = "${service.url}")
 public interface GameService {
-    @RequestMapping(method = POST, value = "/api/v2/game/start")
+    @PostMapping("/api/v2/game/start")
     Game start();
 }
--- services/src/main/java/lt/jrgames/dragonsofmugloar/services/shop/ShopService.java
+++ services/src/main/java/lt/jrgames/dragonsofmugloar/services/shop/ShopService.java
@@ -3,19 +3,17 @@ package lt.jrgames.dragonsofmugloar.services.shop;
 import lt.jrgames.dragonsofmugloar.services.shop.model.PurchaseResult;
 import lt.jrgames.dragonsofmugloar.services.shop.model.ShopItem;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
 @FeignClient(name = "ShopService", url = "${service.url}")
 public interface ShopService {
-    @RequestMapping(method = GET, value = "/api/v2/{gameId}/shop")
+    @GetMapping("/api/v2/{gameId}/shop")
     List<ShopItem> availableItems(@PathVariable String gameId);
 
-    @RequestMapping(method = POST, value = "/api/v2/{gameId}/shop/buy/{itemId}")
+    @PostMapping("/api/v2/{gameId}/shop/buy/{itemId}")
     PurchaseResult buyItem(@PathVariable String gameId, @PathVariable String itemId);
 }
